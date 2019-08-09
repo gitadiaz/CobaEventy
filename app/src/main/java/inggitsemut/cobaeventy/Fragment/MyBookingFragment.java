196728fc41bfc89425ab9a84e26000a548f1688f
Gita Diaz <30728116+gitadiaz@users.noreply.github.com>
2019-08-09 13:08:18 +0700

    intent ke profile fragment dan my booking fragment

--- app/src/main/java/inggitsemut/cobaeventy/Fragment/MyBookingFragment.java
+++ app/src/main/java/inggitsemut/cobaeventy/Fragment/MyBookingFragment.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toolbar;
 
+import inggitsemut.cobaeventy.Adapter.ViewPagerMyBookingAdapter;
 import inggitsemut.cobaeventy.R;
 
 public class MyBookingFragment extends Fragment implements View.OnClickListener {
@@ -28,7 +30,23 @@ public class MyBookingFragment extends Fragment implements View.OnClickListener 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_my_booking, container, false);
+
+        toolbar = rootView.findViewById(R.id.toolbarMyBooking);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+
+        tabLayout = rootView.findViewById(R.id.tabLayoutMyBooking);
+        viewPager = rootView.findViewById(R.id.viewPagerMyBooking);
+        ViewPagerMyBookingAdapter adapter = new ViewPagerMyBookingAdapter(getActivity().getSupportFragmentManager());
+        // Adding Fragments
+        adapter.AddFragment(new UnpaidBookingFragment(), "Unpaid");
+        adapter.AddFragment(new PaidBookingFragment(), "Paid");
+        adapter.AddFragment(new CancelledBookingFragment(), "Cancel");
+        // Adapter setup
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
         return rootView;
+
     }
 
 
