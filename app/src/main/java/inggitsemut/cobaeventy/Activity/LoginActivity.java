274566f5087bc7393ee0a811da5f8b92495edb52
Gita Diaz <30728116+gitadiaz@users.noreply.github.com>
2019-08-13 11:04:50 +0700

    facebook login, tapi belum rampung

--- app/src/main/java/inggitsemut/cobaeventy/Activity/LoginActivity.java
+++ app/src/main/java/inggitsemut/cobaeventy/Activity/LoginActivity.java
@@ -1,18 +1,38 @@
 package inggitsemut.cobaeventy.Activity;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
 import inggitsemut.cobaeventy.R;
 
 public class LoginActivity extends AppCompatActivity {
-    Button btnSignIn;
-    TextView btnSignUp;
+    private Button btnSignIn;
+    private TextView btnSignUp;
+    private LoginButton btnLoginFacebook;
+
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +41,31 @@ public class LoginActivity extends AppCompatActivity {
 
         btnSignIn = findViewById(R.id.btn_sign_in);
         btnSignUp = findViewById(R.id.btn_sign_up);
+        btnLoginFacebook = findViewById(R.id.login_button_facebook);
+
+        callbackManager = CallbackManager.Factory.create();
+        btnLoginFacebook.setReadPermissions(Arrays.asList("email", "public_profile"));
+
+        btnLoginFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+//                loaduserProfile(AccessToken.getCurrentAccessToken());
+                Intent intentLogin = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intentLogin);
+                finish();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,4 +84,49 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    AccessTokenTracker tokenTracke = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if (currentAccessToken==null){
+                Toast.makeText(LoginActivity.this, "User log out", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                loaduserProfile(currentAccessToken);
+            }
+        }
+    };
+
+    private void loaduserProfile(AccessToken newAccessToken){
+        GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+                try {
+                    String first_name = object.getString("first_nanme");
+                    String last_name = object.getString("last_name");
+                    String email = object.getString("email");
+                    String id = object.getString("id");
+                    String image_url = "https://graph.facebook.com/"+id+"/picture?type=normal";
+
+                    Intent intentLogin = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intentLogin);
+
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Bundle parameters = new Bundle();
+    }
+
+
 }
