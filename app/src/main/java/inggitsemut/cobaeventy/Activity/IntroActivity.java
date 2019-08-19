a158b46d7e1c65c9ccd8875a571f86d72b2b1886
Gita Diaz <30728116+gitadiaz@users.noreply.github.com>
2019-08-19 14:28:50 +0700

    dynamic view pager for intro slider

--- app/src/main/java/inggitsemut/cobaeventy/Activity/IntroActivity.java
+++ app/src/main/java/inggitsemut/cobaeventy/Activity/IntroActivity.java
@@ -10,13 +10,20 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import inggitsemut.cobaeventy.Adapter.IntroViewPagerAdapter;
+import inggitsemut.cobaeventy.Api.ConfigUtils;
+import inggitsemut.cobaeventy.Api.Service;
 import inggitsemut.cobaeventy.Models.ScreenItem;
+import inggitsemut.cobaeventy.Models.ScreenItemList;
 import inggitsemut.cobaeventy.R;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -46,81 +53,87 @@ public class IntroActivity extends AppCompatActivity {
         btnGetStarted = findViewById(R.id.btn_get_started);
         btnAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.button_animation);
 
-        // fill list screen
+        // setup viewpager
+        viewPagerIntro = findViewById(R.id.viewPagerIntroSlider);
 
-//        Service service = getClient().create(Service.class);
-//
-//        final Call<ScreenItem> callIntroSlider = service.getIntroSlider();
-//
-//        callIntroSlider.enqueue(new Callback<ScreenItem>() {
-//            @Override
-//            public void onResponse(Call<ScreenItem> call, Response<ScreenItem> response) {
+        // fill list screen
+        Service service = ConfigUtils.getClient().create(Service.class);
+
+        final Call<ScreenItemList> callIntroSlider = service.getIntroSlider();
+
+        callIntroSlider.enqueue(new Callback<ScreenItemList>() {
+            @Override
+            public void onResponse(Call<ScreenItemList> call, Response<ScreenItemList> response) {
 //                final List<ScreenItem> mList = new ArrayList<>();
 //                for (i=0; i<response.body(); i++){
 //                    mList.add(new ScreenItem(response.body().getTitle(), response.body().getDescription(), R.drawable.scan));
-//                }
-//
-//            }
-//
-//            @Override
-//            public void onFailure(Call<ScreenItem> call, Throwable t) {
-//                Toast.makeText(IntroActivity.this, "gagal", Toast.LENGTH_LONG).show();
-//            }
-//
-//
-//        });
-        final List<ScreenItem> mList = new ArrayList<>();
-        mList.add(new ScreenItem("Scan", "Lorem ipsum dolor sit amet, eu nihil prompta vix, ne tacimates imperdiet vel, eu vis simul veniam regione. Postulant efficiendi sed at, eius liber bonorum id quo, mei ad noluisse patrioque. ", "https://images.genius.com/5b8ea872faf17e09914610774e46dbad.1000x1000x1.jpg"));
-        mList.add(new ScreenItem("Search", "Lorem ipsum dolor sit amet, eu nihil prompta vix, ne tacimates imperdiet vel, eu vis simul veniam regione. Postulant efficiendi sed at, eius liber bonorum id quo, mei ad noluisse patrioque.", "https://images.genius.com/5b8ea872faf17e09914610774e46dbad.1000x1000x1.jpg"));
-        mList.add(new ScreenItem("Navigate", "Lorem ipsum dolor sit amet, eu nihil prompta vix, ne tacimates imperdiet vel, eu vis simul veniam regione. Postulant efficiendi sed at, eius liber bonorum id quo, mei ad noluisse patrioque.", "https://images.genius.com/5b8ea872faf17e09914610774e46dbad.1000x1000x1.jpg"));
 
-        // setup viewpager
-        viewPagerIntro = findViewById(R.id.viewPagerIntroSlider);
-        introViewPagerAdapter  = new IntroViewPagerAdapter(this, mList);
-        viewPagerIntro.setAdapter(introViewPagerAdapter);
+                //getting list of heroes
+                final ArrayList<ScreenItem> screenItems = response.body().getScreenItemList();
 
-        // setup tab layout with view pager
-        tabLayoutIndicator.setupWithViewPager(viewPagerIntro);
+                // setup viewpager
+                introViewPagerAdapter  = new IntroViewPagerAdapter(getApplicationContext(), screenItems);
+                viewPagerIntro.setAdapter(introViewPagerAdapter);
 
-        // btn next set on click listener
-        btnNext.setOnClickListener(new View.OnClickListener() {
+                // setup tab layout with view pager
+                tabLayoutIndicator.setupWithViewPager(viewPagerIntro);
+
+                // btn next set on click listener
+                btnNext.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        position = viewPagerIntro.getCurrentItem();
+                        if (position < screenItems.size()){
+                            position++;
+                            viewPagerIntro.setCurrentItem(position);
+                        }
+
+                        if (position == screenItems.size()-1){
+                            loadLastScreen();
+                        }
+                    }
+                });
+
+                // tab layout add change listener
+                tabLayoutIndicator.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+                    @Override
+                    public void onTabSelected(TabLayout.Tab tab) {
+                        if (tab.getPosition() == screenItems.size()-1){
+                            loadLastScreen();
+                        }
+                        else if (tab.getPosition() != screenItems.size()-1){
+                            reloadScreen();
+                        }
+                    }
+
+                    @Override
+                    public void onTabUnselected(TabLayout.Tab tab) {
+
+                    }
+
+                    @Override
+                    public void onTabReselected(TabLayout.Tab tab) {
+
+                    }
+                });
+                }
+
             @Override
-            public void onClick(View v) {
+            public void onFailure(Call<ScreenItemList> call, Throwable t) {
+                Toast.makeText(IntroActivity.this, "gagal", Toast.LENGTH_LONG).show();
 
-                position = viewPagerIntro.getCurrentItem();
-                if (position < mList.size()){
-                    position++;
-                    viewPagerIntro.setCurrentItem(position);
-                }
-
-                if (position == mList.size()-1){
-                    loadLastScreen();
-                }
             }
+
         });
+//        final List<ScreenItem> mList = new ArrayList<>();
+//        mList.add(new ScreenItem("Scan", "Lorem ipsum dolor sit amet, eu nihil prompta vix, ne tacimates imperdiet vel, eu vis simul veniam regione. Postulant efficiendi sed at, eius liber bonorum id quo, mei ad noluisse patrioque. ", "https://images.genius.com/5b8ea872faf17e09914610774e46dbad.1000x1000x1.jpg"));
+//        mList.add(new ScreenItem("Search", "Lorem ipsum dolor sit amet, eu nihil prompta vix, ne tacimates imperdiet vel, eu vis simul veniam regione. Postulant efficiendi sed at, eius liber bonorum id quo, mei ad noluisse patrioque.", "https://images.genius.com/5b8ea872faf17e09914610774e46dbad.1000x1000x1.jpg"));
+//        mList.add(new ScreenItem("Navigate", "Lorem ipsum dolor sit amet, eu nihil prompta vix, ne tacimates imperdiet vel, eu vis simul veniam regione. Postulant efficiendi sed at, eius liber bonorum id quo, mei ad noluisse patrioque.", "https://images.genius.com/5b8ea872faf17e09914610774e46dbad.1000x1000x1.jpg"));
 
-        // tab layout add change listener
-        tabLayoutIndicator.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                if (tab.getPosition() == mList.size()-1){
-                    loadLastScreen();
-                }
-                else if (tab.getPosition() != mList.size()-1){
-                    reloadScreen();
-                }
-            }
 
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
 
-            }
 
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
 
         // btn get started set on click listener
         btnGetStarted.setOnClickListener(new View.OnClickListener() {
